c65e13f6147e11bdfc3010576537527cef44f178
fajrulalam <fajrulalam01@gmail.com>
2026-06-25 09:02:24 +0700

    changed the ustadzah's number

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -356,14 +356,14 @@ export default function Home() {
                                             {/* Lucide Icon Placeholder: smartphone */}
                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-amber-700 dark:text-amber-400"><rect width="14" height="20" x="5" y="2" rx="2" ry="2"/><path d="M12 18h.01"/></svg>
                                         </div>
-                                        <p className="text-amber-900 dark:text-amber-300 font-medium">0852-3247-9151 (Ustadzah Rani)</p>
+                                        <p className="text-amber-900 dark:text-amber-300 font-medium">+62 857-8454-1496 (Ustadzah Alivia)</p>
                                     </div>
                                     <div className="flex items-center gap-2">
                                         <div className="p-1.5 bg-amber-100 dark:bg-amber-900/30 rounded-full flex-shrink-0">
                                             {/* Lucide Icon Placeholder: smartphone */}
                                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-amber-700 dark:text-amber-400"><rect width="14" height="20" x="5" y="2" rx="2" ry="2"/><path d="M12 18h.01"/></svg>
                                         </div>
-                                        <p className="text-amber-900 dark:text-amber-300 font-medium">0812-3396-8261 (Ustadzah Nuril)</p>
+                                        <p className="text-amber-900 dark:text-amber-300 font-medium">+62 896‑7896‑2686 (Ustadzah Shafira)</p>
                                     </div>
                                     {/* Email Contact */}
                                     {/*<div className="flex items-center gap-2">*/}
